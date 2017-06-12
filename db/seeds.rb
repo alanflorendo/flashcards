f9f1db357ee2b2adbb1abebbd7eb3fa18d0db6db
Florendo Alan <magnet-dev+fa@asynchrony.com>
2017-06-11 19:33:28 -0500

    complicate seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,40 @@
 require 'csv'
+class Seeder
+  DECK_NAMES = %w(european_capitals
+                  oscars_best_picture_by_year
+                  presidents_by_order
+                  presidents_by_term
+                  us_state_capitals
+                  canada_province_capitals
+  )
 
-csv_text = File.read(Rails.root.join('db', 'cards', 'country_capitals.txt'))
-csv = CSV.parse(csv_text, headers: true)
+  def seed
+  DECK_NAMES.each { |d| make_deck_and_cards(d) }
+  end
 
-csv.each do |row|
-  card = row.to_hash
-  c = Card.create(front: card['Front'], back: card['Back'], review: false)
+  def make_deck_and_cards(filename)
+    csv_text = File.read(Rails.root.join('db', 'cards', "#{filename}.txt"))
+    deck_name = filename.humanize.titleize
+    deck = Deck.find_by(name: deck_name)
+    if deck
+      puts "#{filename} already exists"
+    else
+      deck = Deck.create(name: deck_name)
+      csv = CSV.parse(csv_text, headers: true)
+      puts "Creating deck for #{deck_name}"
+      make_cards(deck, csv)
+    end
+  end
+
+  def make_cards(deck, csv)
+    csv.each do |row|
+      card = row.to_hash
+      c = Card.create(front: card['Front'],
+                      back: card['Back'], review: false,
+                      deck_id: deck.id)
+    end
+    puts "Created #{csv.length} cards for #{deck.name}"
+  end
 end
+
+Seeder.new.seed
